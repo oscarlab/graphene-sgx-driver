b01be4b9e20815d2fde1cb140ae0d68f80c954f0
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2020-05-05 18:09:28 -0700

    fixup! Add support for the DCAP SGX driver versions 1.6 and newer

--- gsgx.h
+++ gsgx.h
@@ -23,33 +23,35 @@
 #define GSGX_FILE  "/dev/gsgx"
 #define GSGX_MINOR MISC_DYNAMIC_MINOR
 
-/* SGX leaf instruction return values */
-#define SGX_SUCCESS             0
+/* Graphene needs the below subset of SGX instructions' return values */
+#ifndef SGX_INVALID_SIG_STRUCT
 #define SGX_INVALID_SIG_STRUCT  1
+#endif
+
+#ifndef SGX_INVALID_ATTRIBUTE
 #define SGX_INVALID_ATTRIBUTE   2
-#define SGX_BLKSTATE            3
+#endif
+
+#ifndef SGX_INVALID_MEASUREMENT
 #define SGX_INVALID_MEASUREMENT 4
-#define SGX_NOTBLOCKABLE        5
-#define SGX_PG_INVLD            6
-#define SGX_LOCKFAIL            7
+#endif
+
+#ifndef SGX_INVALID_SIGNATURE
 #define SGX_INVALID_SIGNATURE   8
-#define SGX_MAC_COMPARE_FAIL    9
-#define SGX_PAGE_NOT_BLOCKED    10
-#define SGX_NOT_TRACKED         11
-#define SGX_VA_SLOT_OCCUPIED    12
-#define SGX_CHILD_PRESENT       13
-#define SGX_ENCLAVE_ACT         14
-#define SGX_ENTRYEPOCH_LOCKED   15
+#endif
+
+#ifndef SGX_INVALID_EINITTOKEN
 #define SGX_INVALID_EINITTOKEN  16
-#define SGX_PREV_TRK_INCMPL     17
-#define SGX_PG_IS_SECS          18
+#endif
+
+#ifndef SGX_INVALID_CPUSVN
 #define SGX_INVALID_CPUSVN      32
-#define SGX_INVALID_ISVSVN      64
-#define SGX_UNMASKED_EVENT      128
-#define SGX_INVALID_KEYNAME     256
+#endif
 
 /* SGX_INVALID_LICENSE was renamed to SGX_INVALID_EINITTOKEN in SGX driver 2.1:
  *   https://github.com/intel/linux-sgx-driver/commit/a7997dafe184d7d527683d8d46c4066db205758d */
+#ifndef SGX_INVALID_LICENSE
 #define SGX_INVALID_LICENSE     SGX_INVALID_EINITTOKEN
+#endif
 
 #endif /* __ARCH_GSGX_H__ */
